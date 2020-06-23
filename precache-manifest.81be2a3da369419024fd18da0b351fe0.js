fbd9d4fea83d63eaf2269da1b88bf667f6233e2f
Mike Solomon <mike.solomon@zapier.com>
2020-06-23 15:22:07 -0700

    Updates

---
+++ precache-manifest.81be2a3da369419024fd18da0b351fe0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30c31f606606e53f18258e1c2f530741",
+    "url": "/kana-typing/index.html"
+  },
+  {
+    "revision": "fe13a0cfd1775e15a548",
+    "url": "/kana-typing/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d41c12dc1ed0c70b1e52",
+    "url": "/kana-typing/static/js/2.a8b163fc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/kana-typing/static/js/2.a8b163fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe13a0cfd1775e15a548",
+    "url": "/kana-typing/static/js/main.628cdbdd.chunk.js"
+  },
+  {
+    "revision": "d611fed3592f044c8cec",
+    "url": "/kana-typing/static/js/runtime-main.ec722218.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/kana-typing/static/media/logo.5d5d9eef.svg"
+  }
+]);
